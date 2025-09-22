9e8e6d7220da1134f2cc854761e25a7f2d1b503a
Vladimir Budkevich <budkevich.vladimir@gmail.com>
2025-09-22 17:10:59 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="text-white p-4">Header</header>
+      <main className="flex-grow p-4">
+        <div className="flex justify-center items-center gap-4">
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        <h1 className="text-3xl font-bold underline my-class">Hello world!</h1>
+      </main>
+      <footer className="text-white p-4">Футер</footer>
+    </div>
+  );
+}
+
+export default App;
