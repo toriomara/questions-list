7fee73f07821480e571b9a08d9bde68bf04df0f3
Vladimir Budkevich <budkevich.vladimir@gmail.com>
2025-11-11 20:07:45 +0300

    feat: base finished_5

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "questionsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
   }),
   endpoints: () => ({}),
-  tagTypes: ["Question", "Questions", "Skills", "Directions"],
+  tagTypes: ["Question", "Questions", "Skills", "Specializations"],
 });
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,14 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
+    proxy: {
+      "/questions": { target: "https://api.yeatwork.ru", changeOrigin: true },
+      "/specializations": {
+        target: "https://api.yeatwork.ru",
+        changeOrigin: true,
+      },
+      "/skills": { target: "https://api.yeatwork.ru", changeOrigin: true },
+    },
   },
   resolve: {
     alias: {
