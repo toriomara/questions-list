909e22f3b7fa5b1c159047910b854d5c6434efa4
Vladimir Budkevich <budkevich.vladimir@gmail.com>
2025-11-12 22:04:44 +0300

    feat:feat: base finished_18

--- api/index.ts
+++ api/index.ts
@@ -24,7 +24,15 @@
 //   }
 // }
 
-export default function handler(req, res) {
-  const path = req.url.replace('/api/', ''); // 'specializations'
-  res.json({ message: "Proxy работает!", path });
+export default async function handler(req, res) {
+  const path = req.url.replace("/api/", "");
+  const url = `https://api.yeatwork.ru/${path}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed" });
+  }
 }
