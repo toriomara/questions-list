0fd7e8b617db42cb1be6a430db0aadcd1dd884e2
Vladimir Budkevich <budkevich.vladimir@gmail.com>
2025-11-12 19:49:12 +0300

    feat:feat: base finished_11

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,29 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   base: "/",
   server: {
     port: 3000,
+    // proxy: {
+    //   "/questions": { target: "https://api.yeatwork.ru", changeOrigin: true },
+    //   "/specializations": {
+    //     target: "https://api.yeatwork.ru",
+    //     changeOrigin: true,
+    //   },
+    //   "/skills": { target: "https://api.yeatwork.ru", changeOrigin: true },
+    // },
     proxy: {
-      "/questions": { target: "https://api.yeatwork.ru", changeOrigin: true },
-      "/specializations": {
-        target: "https://api.yeatwork.ru",
+      "/api": {
+        target:
+          mode === "production"
+            ? "https://api.yeatwork.ru"
+            : "http://localhost:3000",
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
       },
-      "/skills": { target: "https://api.yeatwork.ru", changeOrigin: true },
     },
   },
   resolve: {
@@ -27,4 +38,23 @@ export default defineConfig({
       "@widgets": path.resolve(__dirname, "./src/widgets"),
     },
   },
-});
+}));
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// export default defineConfig(({ mode }) => ({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target:
+//           mode === "production"
+//             ? "http://api.example.com"
+//             : "http://localhost:3000",
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//         secure: false,
+//       },
+//     },
+//   },
+// }));
